d94344a7cd87409349ea49ad5ad2a83a48ca4eb8
Amit122-Bytes <amitmeta122@gmail.com>
2021-08-07 01:49:45 +0530

    methodoverloading

--- src/OOPS/allmethods.java
+++ src/OOPS/allmethods.java
@@ -1,14 +1,44 @@
 package OOPS;
 
+
 public class allmethods {
 
-	public static void main(String[] args) {
-		
-		// a methods may not take parameters example-empty parament
-		
 		int a;
 		int b;
 		
+//		1)//a methods may not take parameters example-empty parameter
+		//4)// method returns some value
+//		void sum()
+//		{
+//			System.out.println(a+b);
+//		}
+		
+//		void sum1(int x, int y)
+//		{
+//			a=x;
+//			b=y;
+//			System.out.println(a+b);
+//		}
+		
+		int sum()
+		{
+			return(a+b);
+		}
+	
+	public static void main(String[] args) {
+		
+		
+		allmethods s1=new allmethods();
+//		s1.a=100;
+//		s1.b=200;//1)a methods may not take parameters example-empty parameter
+//		s1.sum1();//4) method returns some value
+//		
+//		s1.sum1(100, 200);2)// method takes permission
+		
+//		s1.a=100;// 3)method returns some value
+//		s1.b=200;
+//		int r=s1.sum();
+//		System.out.println(r);
 		
 
 	}
---
+++ src/OOPS/methodoverloading.java
@@ -0,0 +1,11 @@
+package OOPS;
+
+public class methodoverloading {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
